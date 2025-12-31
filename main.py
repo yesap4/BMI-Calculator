c9ebe7afc8b1e6f913b7bd77098567ebc0ff6980
Anant Prakash <yes4ap@gmail.com>
2025-12-31 10:13:10 +0000

    Implement BMI Calculator functionality with Streamlit

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
+import streamlit as st 
+import pandas as pd
+
+st.title('BMI Calculator')
+st.write('## "BMI" stands for Body Mass Index')
+
+height = st.slider('Enter your Height (in cms)',100,250,175)
+
+weight = st.slider('Enter your weight (in kgs)',40,200,70)
+
+bmi = weight / ((height/100 ** 2))
+
+st.write(f'### Height = {height}')
+
+st.write(f'### Weight = {weight} ')
+
+st.write(f'### BMI = {bmi:.2f}')
+
+st.write('## BMI Categories')
+
+st.write('### - Underweight : BMI less than 18.5')
+
+st.write('### - Normal : BMI between 18.5 and 24.9')
+
+st.write('### - Overweight : BMI between 25 and 29.9')
+
+st.write('### - Obesity : BMI 30 or greater')
 
